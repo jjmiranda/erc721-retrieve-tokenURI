33a82fdb100815d80ff904b991a6203ffd007c2b
JJMiranda <jjmiranda@outlook.com>
2024-02-02 13:32:36 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # erc721-retrieve-tokenURI
 Example for getting NFT tokenURI
+
+use this command to use .env keys:
+`
+ts-node -r dotenv/config tokenURI.ts
+`
---
+++ tokenURI.ts
@@ -0,0 +1,35 @@
+import { ethers } from "ethers";
+
+
+const RPC = String(process.env.RPC);
+// The Contract interface
+const abi = [
+    "function tokenURI(uint256 _tokenId) external view returns (string)",
+];
+
+const provider = new ethers.JsonRpcProvider(RPC);
+
+// the contract interface         
+const contractAddress = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"; //BAYC contract address
+
+// We connect to the Contract using a Provider, so we will only
+// have read-only access to the Contract
+const contract = new ethers.Contract(contractAddress, abi, provider);
+
+
+// simple sleep function
+function promiseSleep(milliseconds:number) {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+(async () => {
+    try {
+        await promiseSleep(2.5 * 1000);
+
+        const currentValue = await contract.tokenURI(101);
+        console.log(currentValue);
+
+    } catch (e) {
+        console.error('Contract tokenURI failed: ', e);
+      }
+})();
